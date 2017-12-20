419cc1fb60b7c84d2cdddc4d51b13046de8c4f59
Diego Bonfim <fbm.diego@gmail.com>
2017-12-20 18:20:35 -0200

    Add event module

---
+++ event/src/main/java/kafkastore/event/topology/OrderTopology.java
@@ -0,0 +1,15 @@
+package kafkastore.event.topology;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OrderTopology {
+
+
+    public boolean createToplogy(){
+
+
+
+        return false;
+    }
+}
