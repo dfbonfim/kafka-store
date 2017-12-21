d19762fd433b4ff33e299fe0b98196036fc76f7d
Diego Bonfim <fbm.diego@gmail.com>
2017-12-21 19:18:23 -0200

    Add spring boot to event module

--- event/src/main/java/kafkastore/event/topology/OrderTopology.java
+++ event/src/main/java/kafkastore/event/topology/OrderTopology.java
@@ -1,15 +1,38 @@
 package kafkastore.event.topology;
 
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
 
 @Configuration
 public class OrderTopology {
 
 
+    @Bean
     public boolean createToplogy(){
 
+        final Serde<String> stringSerde = Serdes.String();
 
+        final KStreamBuilder builder = new KStreamBuilder();
+
+        builder.stream(stringSerde, stringSerde, "input")
+                .map((key, value)  -> new KeyValue<>("1",value))
+                .to(stringSerde, stringSerde, "output");
+
+        final KafkaStreams payloadStream = new KafkaStreams(builder, getProperties());
+
+        payloadStream.start();
 
         return false;
+    }
+
+    private Properties getProperties(){
+        return new Properties();
     }
 }
