7e207b0d3e7129f323eefaa2ed43f2b0d13c1550
Diego Bonfim <fbm.diego@gmail.com>
2018-01-30 15:55:56 -0200

    Add debezium events

---
+++ scripts/create_task_debezium_connector.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo 'Start task creation on debezium'
+
+curl -i -X POST \
+   -H "Content-Type:application/json" \
+   -d \
+'{
+   "name":"connector_mysql",
+   "config":{
+      "name":"connector_mysql",
+      "connector.class":"io.debezium.connector.mysql.MySqlConnector",
+      "tasks.max":"1",
+      "database.hostname":"mysql",
+      "database.port":"3306",
+      "database.user":"root",
+      "database.password":"root",
+      "database.server.name":"debezium",
+      "database.history.kafka.bootstrap.servers":"kafka:9092",
+      "database.history.kafka.topic":"debezium.schema.changes",
+      "snapshot.mode":"schema_only"
+   }
+}' \
+ 'http://localhost:8083/connectors'
+
+echo ""
+echo 'Finished task creation on debezium'
